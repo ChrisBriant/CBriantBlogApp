922bef216a2e792d4eeb55d722d09a69a7a81502
chrisbriant <chrisbriant@github.com>
2020-01-07 18:14:25 +0000

    cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const connectDB = require('./DB/Connection');
 const app = express();
+const cors = require('cors')
 
 connectDB();
 app.use(express.json({ extended: false }));
 app.use('/', require('./Api/Blog'));
+app.use(cors());
 const Port = process.env.PORT || 3000;
 
 app.listen(Port, () => console.log('Server started on '+ Port ));
